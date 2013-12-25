e32a953f65bed603d3215cff49eb807beed45f74
Tomas Doran <tdoran@yelp.com>
2013-12-25 21:03:58 +0000

    Initial code sketch

--- agent/aptpackagerepsotory.ddl
+++ agent/aptpackagerepsotory.ddl
@@ -1,8 +1,43 @@
 metadata :name => "aptpackagerepsotory",
-         :description => "%DESCRIPTION%",
-         :author => "%AUTHOR%",
-         :license => "%LICENSE%",
-         :version => "%VERSION%",
-         :url => "%URL%",
-         :timeout => %TIMEOUT%
+         :description => "Trivial apt package repository maintainance",
+         :author => "Tomas Doran",
+         :license => "Apache2",
+         :version => "0.0.1",
+         :url => "https://github.com/bobtfish/mcollective-agent-aptpackagerepsotory",
+         :timeout => 60
+
+action "add", :description => "Add a package to the repository" do
+  display :always
+
+  input :uri,
+    :description => 'uri',
+    :display_as => 'URI to get the new package from',
+    :optional => false,
+    :type => :string,
+    :prompt => 'URI to get package from?',
+    :maxlength => 512
+
+  input :dist,
+    :description => "distribution",
+    :display_as => 'the distribution to add the package to',
+    :optional => false,
+    :type => :string,
+    :prompt => 'Distribution (precise/squeeze?)',
+    :validation => '^(precise|squeeze)$',
+    :maxlength => 7
+
+  input :arch,
+    :description => "architecture",
+    :display_as => "the architecture to add the package for (amd64/i386)",
+    :optional => false,
+    :type => :string,
+    :prompt => 'Architecture (i386/amd64?)',
+    :validation => '^(i386|amd64)$',
+    :maxlength => 7 
+
+  output :status,
+    :description => "The command exit code",
+    :display_as  => "Exit code"
+
+end
 
--- agent/aptpackagerepsotory.rb
+++ agent/aptpackagerepsotory.rb
@@ -1,7 +1,79 @@
+require 'net/http'
+require 'tempfile'
+
 module MCollective
   module Agent
     class Aptpackagerepsotory<RPC::Agent
+      action "update" do
+        validate :dist, String
+        validate :dist, :shellsafe
+        validate :arch, String
+        validate :arch, :shellsafe
+        tmpfile = get_uri(request[:uri])
+        File.rename tmpfile.path "#{package_root}/dists/#{request[:dist]}/main/binary-#{request[:arch]}/"
+        fixup_metadata
+      end
 
+      def get_uri(url)
+        uri = URI.parse(url)
+        temp = Tempfile.new('mco-agent-aptpackagerepository')
+        Net::HTTP.start(uri.host,uri.port) do |http| 
+          http.request_get(uri.path) do |res| 
+            res.read_body do |seg|
+              temp.write(seg)
+              sleep 0.005 # hack
+            end
+          end
+        end
+        temp.close
+        temp
+      end
+
+      def package_root
+        '/var/www/packages.lemonparty.goatse.co.uk/htdocs'
+      end
+
+      def dists
+        {
+            :precise => [ :i386, :amd64 ],
+            :squeeze => [ :i386 ]
+        }
+      end
+
+      def gpg_key
+        '58E2174D'
+      end
+
+      def fixup_metadata
+        Dir.chdir(package_root) do
+          dists.each do |name, archs|
+            archs.each do |arch|
+              dir = "dists/#{name.to_s}/main/binary-#{arch.to_s}"
+              system("dpkg-scanpackages -m #{dir} /dev/null > #{dir}/Packages")
+              system("dpkg-scansources #{dir} /dev/null > #{dir}/Sources")
+
+              system("bzip2 -c9 #{dir}/Packages > #{dir}/Packages.bz2")
+              system("gzip -c9  #{dir}/Packages > #{dir}/Packages.gz")
+              system("bzip2 -c9 #{dir}/Sources  > #{dir}/Sources.bz2")
+              system("gzip -c9  #{dir}/Sources  > #{dir}/Sources.gz")
+
+            end
+            File.open("dists/#{name.to_s}/Release", 'w') do |f|
+              f.puts "Suite: #{name}"
+              f.puts "Codename: #{name}"
+              f.puts "Components: main"
+              f.puts "Architectures: #{archs.join(' ')}"
+              f.puts `apt-ftparchive release dists/#{name.to_s}`
+            end
+            system("rm -f dists/#{name.to_s}/Release.gpg")
+            system("gpg --homedir=/root/.gnupg/ -abs --default-key #{gpg_key} -o dists/#{name.to_s}/Release.gpg dists/#{name.to_s}/Release")
+          end
+
+          system("sudo chgrp -R admin *")
+          system("sudo chmod -R g+w *")
+        end
+      end
     end
   end
 end
+
